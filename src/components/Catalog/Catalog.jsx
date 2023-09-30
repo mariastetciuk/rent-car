78f22368d2b3f3120764fee4e82ec808a3032b29
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-09-30 23:56:33 +0300

    add spinner

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -4,16 +4,20 @@ import scss from './Catalog.module.scss';
 import { useEffect, useState, useContext } from 'react';
 import { getGallery } from 'API/fetch';
 import { ToggleContext } from 'components/SharedLayout/SharedLayout';
+import CircleLoader from "react-spinners/CircleLoader";
+
 
 const Catalog = () => {
   const { page, addPage } = useContext(ToggleContext);
   const [gallary, setGallary] = useState([]);
+  const [isSpinner, setIsSpinner] = useState(true);
 
   useEffect(() => {
     async function featch() {
       try {
         const { data } = await getGallery(1);
         setGallary(prev => [...data]);
+        setIsSpinner(false);
       } catch (error) {
         console.log(error);
       }
@@ -37,6 +41,9 @@ const Catalog = () => {
   return (
     <section className={scss.catalog}>
       <Fillter handleFillter={handleFillter} gallary={gallary} />
+
+{isSpinner && <CircleLoader  className={scss.spinner} size='150px' color='blue' aria-label="Loading Spinner"
+    data-testid="loader" />}
       <CarsList
         handleBtnLoadMore={handleBtnLoadMore}
         gallary={gallary}
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import Container from 'components/Shared/Container';
 
 
 
+
+
 const Footer = () =>{
 
     return (<footer className={scss.footer}>
@@ -11,10 +13,10 @@ const Footer = () =>{
             <div className={scss.footerContainer}>
 <p className={scss.text}>Developer <span className={scss.name}>Mariia Stetsiuk</span> </p>
 <ul className={scss.list}>
-    <li className={scss.item}><a href="https://www.linkedin.com/in/mariia-stetsiuk/" className={scss.link}>
+    <li className={scss.item}><a href="https://www.linkedin.com/in/mariia-stetsiuk/" className={scss.link} target='blank'>
         <AiFillLinkedin size='30px'  className={scss.icon}/>
         </a></li>
-    <li className={scss.item}><a href="https://github.com/mariastetciuk" className={scss.link}>
+    <li className={scss.item}><a href="https://github.com/mariastetciuk" className={scss.link} target='blank'>
         <AiFillGithub size='30px' className={scss.icon}/>
         </a></li>
 </ul>
